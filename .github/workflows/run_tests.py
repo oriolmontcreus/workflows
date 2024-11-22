6bd4440157993c9c117b51b10927041ba1cba61b
oriolmontcreus <licensedgamer641319@gmail.com>
2024-11-22 18:26:16 +0100

    ffff

--- .github/workflows/run_tests.py
+++ .github/workflows/run_tests.py
@@ -1,7 +1,7 @@
-
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 class FlaskAppTest(unittest.TestCase):
 
@@ -16,22 +16,22 @@ class FlaskAppTest(unittest.TestCase):
 
     def test_addition(self):
         driver = self.driver
-        number1 = driver.find_element_by_id("number1")
-        number2 = driver.find_element_by_id("number2")
+        number1 = driver.find_element(By.ID, "number1")
+        number2 = driver.find_element(By.ID, "number2")
         number1.send_keys("10")
         number2.send_keys("20")
         number2.send_keys(Keys.RETURN)
-        result = driver.find_element_by_id("result").get_attribute("value")
+        result = driver.find_element(By.ID, "result").get_attribute("value")
         self.assertEqual(result, "30.0")
 
     def test_invalid_input(self):
         driver = self.driver
-        number1 = driver.find_element_by_id("number1")
-        number2 = driver.find_element_by_id("number2")
+        number1 = driver.find_element(By.ID, "number1")
+        number2 = driver.find_element(By.ID, "number2")
         number1.send_keys("abc")
         number2.send_keys("20")
         number2.send_keys(Keys.RETURN)
-        error = driver.find_element_by_id("error").text
+        error = driver.find_element(By.ID, "error").text
         self.assertEqual(error, "Tots els camps han de contenir números vàlids!")
 
 if __name__ == "__main__":
