af59ad7dbcd33aa4cecb2c77bd0b0de6a202efb2
oriolmontcreus <licensedgamer641319@gmail.com>
2024-11-22 17:37:57 +0100

    test 7

--- .github/workflows/test_functional.py
+++ .github/workflows/test_functional.py
@@ -14,6 +14,7 @@ class TestWebApp(unittest.TestCase):
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         service = Service("/usr/local/bin/chromedriver")
+        options.binary_location = "/usr/local/bin/chromedriver"
         cls.driver = webdriver.Chrome(service=service, options=options)
         cls.driver.get("http://127.0.0.1:5000/")
 
