3f46dfc2381729ac174355289922cbdef1e5f0b5
oriolmontcreus <licensedgamer641319@gmail.com>
2024-11-22 18:34:38 +0100

    kk

--- .github/workflows/run_tests.py
+++ .github/workflows/run_tests.py
@@ -11,6 +11,8 @@ class FlaskAppTest(unittest.TestCase):
         options = Options()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
+        options.add_argument('--remote-debugging-pipe')
+        options.add_argument("--disable-dev-shm-usage")
         cls.driver = webdriver.Chrome()
         cls.driver.get("http://127.0.0.1:5000")
 
