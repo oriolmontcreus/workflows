e107f1dd1c16ce1158e7415f98fa8e3ea6097d0e
oriolmontcreus <licensedgamer641319@gmail.com>
2024-11-22 17:12:45 +0100

    test

---
+++ .github/workflows/test_functional.py
@@ -0,0 +1,43 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+class ProvaAplicacioFlask(unittest.TestCase):
+
+    def setUp(self):
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.get("http://127.0.0.1:5000/")
+
+    def test_suma(self):
+        driver = self.driver
+        numero1 = driver.find_element(By.ID, "number1")
+        numero2 = driver.find_element(By.ID, "number2")
+        numero1.send_keys("5")
+        numero2.send_keys("10")
+        numero2.send_keys(Keys.RETURN)
+        
+        resultat = driver.find_element(By.ID, "result").get_attribute("value")
+        self.assertEqual(resultat, "15.0")
+
+    def test_invalid_input(self):
+        driver = self.driver
+        numero1 = driver.find_element(By.ID, "number1")
+        numero2 = driver.find_element(By.ID, "number2")
+        numero1.send_keys("abc")
+        numero2.send_keys("xyz")
+        numero2.send_keys(Keys.RETURN)
+        
+        error_message = driver.find_element(By.XPATH, "//p[@style='color: red;']").text
+        self.assertEqual(error_message, "Tots els camps han de contenir números vàlids!")
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
